4719b18dec9d7a518b79bdd36729005007453ef0
anhtuai1607-design <anhtuai16.07@gmail.com>
2026-03-27 08:34:23 +0700

    server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,14 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.ambvbbc.mongodb.net',
+  DB_USER: 'tuan',
+  DB_PASS: '1607',
+  DB_DATABASE: 'shoppingonline',
+
+  EMAIL_USER: 'anhtuai16.07@gmail.com',
+  EMAIL_PASS: 'monynliopjacypdt',
+
+  JWT_SECRET: 'abc',
+  JWT_EXPIRES: '3287556289523977687' 
+};
+
+module.exports = MyConstants;
